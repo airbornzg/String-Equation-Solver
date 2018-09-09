839f500be30bbbe2ac99971a37ce92d0e880518a
Zuguang Chen <airborn@qq.com>
2018-09-09 20:27:37 +1000

    Rearrange the methods and formating

--- Assignment1_RegexVer/Assignment1_Ver3/assignment1_ver3/Program.cs
+++ Assignment1_RegexVer/Assignment1_Ver3/assignment1_ver3/Program.cs
@@ -1,11 +1,63 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-
-
+/*
+ |     Course:  Application Development with .NET
+ |     Assignment:  Assignment 1
+ |     Author:  Zuguang Chen
+ |     Student Number:  13106594
+ |     Language:  C#
+ |     Due Date:  17th Sep, 2018
+ * 
+*/
 namespace assignment1_ver3
 {
     class Solver
     {
+        private double x = 0, b = 0, c = 0;
+
+        public Solver(string strLeft, string strRight)
+        {
+            //The regular expression pattern to find the coefficinet
+            string regexB = "([+-][0-9]*)[*]*x(?!\\^)(?![*/])";
+            string regexBD = "([+-]+)([0-9]*)([x]+)([/]+)([+-]*[0-9]+)";
+            string regexC = "(?<![*/]+)([+-][0-9]+)(?!x)(?![(])(?![*])(?![/])(?![0-9]*[*x])(?![*/]*[0-9]+[*/]*)";
+            string regexCM = "([+-]*[0-9]+)([(*]+)([+-]*[0-9]+)(?![x]+)";
+            string regexCD = "([+-]*[0-9]+)([/]+)([+-]*[0-9]+)(?![x]+)";
+
+            //Left equation
+            double bLeft = FindAddMinusCoeff(strLeft, regexB);
+            double cLeft = FindAddMinusCoeff(strLeft, regexC);
+            double bLeftD = FindDivisionCoeff(strLeft, regexBD);
+            double cLeftM = FindMultiCoeff(strLeft, regexCM);
+            double cLeftD = FindDivisionCoeff(strLeft, regexCD);
+
+            //Right equation
+            double bRight = FindAddMinusCoeff(strRight, regexB);
+            double cRight = FindAddMinusCoeff(strRight, regexC);
+            double bRightD = FindDivisionCoeff(strRight, regexBD);
+            double cRightM = FindMultiCoeff(strRight, regexCM);
+            double cRightD = FindDivisionCoeff(strRight, regexCD);
+
+            //Add all the coeffcient parts together and get final coefficient
+            b = (bLeft + bLeftD) - (bRight + bRightD);
+            c = (cLeft + cLeftM + cLeftD) - (cRight + cRightM + cRightD);
+
+            //DivideByZeroException
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                //bx+c=0 general root solver formula
+                x = -(c / b);
+          
+                //Print out the result
+                Console.WriteLine("X = {0}", x);
+                Console.ReadKey();
+            }      
+        }
+
         //Addition and Minus calculation handler
         public double FindAddMinusCoeff(string str, string regex)
         {
@@ -104,14 +156,15 @@ namespace assignment1_ver3
             return coeSum;
         }
     }
+    // Class for main()
     class MainClass
     {
         public static void Main(string[] args)
         {
             //Variable declaration
             string strLeft, strRight;
-            double x = 0, b = 0, c = 0;
             string equ;
+
             //Exception Handling
             try
             {
@@ -127,80 +180,20 @@ namespace assignment1_ver3
                     {
                         equ = args[1];
                     }
+
                     //Check if contains x and = sign
                     if (Regex.IsMatch(equ, "x") && Regex.IsMatch(equ, "=") && Regex.IsMatch(equ, "/0") == false)
                     {
+                        //Print out the input equation
                         Console.WriteLine("The equation is: {0}", equ);
+
                         //Divide the equation into 2 respect to the equal sign
                         string[] equDivided = equ.Split('=');
                         strLeft = equDivided[0];
                         strRight = equDivided[1];
 
-                        //The regular expression pattern to find the coefficinet
-                        string regexB = "([+-][0-9]*)[*]*x(?!\\^)(?![*/])";
-                        string regexBD = "([+-]+)([0-9]*)([x]+)([/]+)([+-]*[0-9]+)";
-                        //string regexBMF = "([+-]+)([0-9]+)([(*]+)([0-9]*)([x]+)";
-                        //string regexBMB = "([+-]+)([0-9]*)([x]+)([*]+)([0-9]+)";
-                        string regexC = "(?<![*/]+)([+-][0-9]+)(?!x)(?![(])(?![*])(?![/])(?![0-9]*[*x])(?![*/]*[0-9]+[*/]*)";
-                        string regexCM = "([+-]*[0-9]+)([(*]+)([+-]*[0-9]+)(?![x]+)";
-                        string regexCD = "([+-]*[0-9]+)([/]+)([+-]*[0-9]+)(?![x]+)";
-
-                        Solver solver = new Solver();
-
-                        //Left equation
-                        double bLeft = solver.FindAddMinusCoeff(strLeft, regexB);
-                        double cLeft = solver.FindAddMinusCoeff(strLeft, regexC);
-
-                        //double bLeftMF = solver.FindMultiCoeff(strLeft, regexBMF);
-                        //double bLeftMB = solver.FindMultiCoeff(strLeft, regexBMB);
-                        double bLeftD = solver.FindDivisionCoeff(strLeft, regexBD);
-                        double cLeftM = solver.FindMultiCoeff(strLeft, regexCM);
-                        double cLeftD = solver.FindDivisionCoeff(strLeft, regexCD);
-
-                        //Right equation
-                        double bRight = solver.FindAddMinusCoeff(strRight, regexB);
-                        double cRight = solver.FindAddMinusCoeff(strRight, regexC);
-
-                        //double bRightMF = solver.FindMultiCoeff(strRight, regexBMF);
-                        //double bRightMB = solver.FindMultiCoeff(strLeft, regexBMB);
-                        double bRightD = solver.FindDivisionCoeff(strRight, regexBD);
-                        double cRightM = solver.FindMultiCoeff(strRight, regexCM);
-                        double cRightD = solver.FindDivisionCoeff(strRight, regexCD);
-
-                        Console.WriteLine("Coefficient bl = {0}", bLeft);
-                        //Console.WriteLine("Coefficient blmf = {0}", bLeftMF);
-                        //Console.WriteLine("Coefficient brmf = {0}", bRightMF);
-                        Console.WriteLine("Coefficient bld = {0}", bLeftD);
-                        Console.WriteLine("Coefficient brd = {0}", bRightD);
-                        Console.WriteLine("Coefficient cl = {0}", cLeft);
-                        Console.WriteLine("Coefficient cr = {0}", cRight);
-                        Console.WriteLine("Coefficient clm = {0}", cLeftM);
-                        Console.WriteLine("Coefficient cld = {0}", cLeftD);
-                        Console.WriteLine("Coefficient crm = {0}", cRightM);
-                        Console.WriteLine("Coefficient crd = {0}", cRightD);
-
-                        //Add all the coeffcient parts together and get final coefficient
-                        b = (bLeft + bLeftD) - (bRight + bRightD);
-                        c = (cLeft + cLeftM + cLeftD) - (cRight + cRightM + cRightD);
-
-                        //DivideByZeroException
-                        if (b == 0)
-                        {
-                            throw new DivideByZeroException();
-                        }
-                        else
-                        {
-                            //bx+c=0 general root solver formula
-                            x = -(c / b);
-
-                            //Print out the coefficient
-                            Console.WriteLine("Coefficient b = {0}", b);
-                            Console.WriteLine("Coefficient c = {0}", c);
-
-                            //Print out the result
-                            Console.WriteLine("Result x = {0}", x);
-                            Console.ReadKey();
-                        }
+                        //Solve this equation
+                        Solver solver = new Solver(strLeft, strRight);
                     }
                     else if (Regex.IsMatch(equ, "/0") == false)
                     {
